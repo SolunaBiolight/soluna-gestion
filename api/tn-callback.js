e533181d3d701c5057084d001cff902f59cf9f64
Soluna <soluna.biolight@gmail.com>
2026-04-21 19:36:43 -0300

    fix private key format

--- api/tn-callback.js
+++ api/tn-callback.js
@@ -14,7 +14,7 @@ function initAdmin() {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n").replace(/"/g, ""),
     }),
   });
   return getFirestore();
